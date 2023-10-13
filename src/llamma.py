56de748da9690860a1d0df6b0eabf8405c8d0841
Thomas Cintra <tncintra@gmail.com>
2023-10-13 16:15:54 -0400

    LLAMMA Swapping

--- src/llamma.py
+++ src/llamma.py
@@ -9,6 +9,31 @@ plt.rcParams.update({'font.size': 10})
 plt.rcParams["axes.spines.top"] = False
 plt.rcParams["axes.spines.right"] = False
 
+class Swap:
+
+    __slots__ = (
+        'in_amount', 
+        'out_amount',
+        'reserves', # reserves[n] = [x, y]
+        'admin_fee', # admin_fee[n] = amt
+        'n1', # band where swap starts
+        'n2', # band where swap ends
+    )
+
+    def __init__(self):
+        self.in_amount = 0
+        self.out_amount = 0
+        self.reserves = defaultdict(lambda: [0,0])
+        self.admin_fee = 0
+        self.n1 = 0
+        self.n2 = 0
+    
+    def __repr__(self):
+        return f"Swap(\nin_amount={self.in_amount}\n out_amount={self.out_amount}\nreserves={self.reserves}\nadmin_fee={self.admin_fee}\nn1={self.n1}\nn2={self.n2})"
+    
+    def __str__(self):
+        return f"Swap(\nin_amount={self.in_amount}\n out_amount={self.out_amount}\nreserves={self.reserves}\nadmin_fee={self.admin_fee}\nn1={self.n1}\nn2={self.n2})"
+
 class LLAMMA:
 
     __slots__ = (
@@ -27,6 +52,8 @@ class LLAMMA:
         'active_band',  # band for current price
         'min_band',  # bands below this are empty
         'max_band',  # bands above this are empty
+        'admin_fees_x', # admin fees collected in stablecoins
+        'admin_fees_y', # admin fees collected in collateral
 
         # === Dependencies/Inputs === #
         'oracle', # Oracle object
@@ -51,6 +78,8 @@ class LLAMMA:
         # Set state variables
         self.bands_x = defaultdict(float)
         self.bands_y = defaultdict(float)
+        self.admin_fees_x = 0
+        self.admin_fees_y = 0
         self.user_shares = defaultdict(lambda: defaultdict(float))
         self.total_shares = defaultdict(float)
         self.active_band = 0
@@ -59,12 +88,137 @@ class LLAMMA:
 
         # Set dependencies
         self.oracle = oracle
+    
+    def _swap(self, amt_in, y_in) -> Swap:
+        """
+        @notice Sell at most amt_in to the AMM.
+        @param amt_in amount of tokens to swap in
+        @param y_in whether collateral is being sold to the AMM (True) or bought from the AMM (False)
+        @return Swap object
+        TODO Account for skipping ticks
+        """
+        s = Swap()
+        s.n1 = s.n2 = self.active_band
 
-    def swap(self, amt_in, amt_out, tkn_in, tkn_out):
+        in_amount_left = amt_in
+
+        x, y = self.bands_x[s.n2], self.bands_y[s.n2]
+
+        for _ in range(self.MAX_TICKS):
+
+            n = s.n2
+            s.reserves[n] = [x, y] # Init
+
+            if y_in:
+                # Going right ->
+                
+                if x != 0:
+                    assert self.f(n) > 0
+                    y_dest = (self.inv(n) / self.f(n) - self.g(n)) - y # Amt required to clear this band
+                    dy = y_dest / (1 - self.fee) # Amt required to clear this band + fee
+
+                    if dy >= in_amount_left:
+                        # This is the last band
+                        y_dest = in_amount_left * (1 - self.fee)
+                        x_left = self.inv(n) / (self.g(n) + (y + y_dest)) - self.f(n) + EPSILON
+                        assert 0 < x_left < x 
+                        admin_fee = (in_amount_left - y_dest) * self.admin_fee
+                        # Updates
+                        s.in_amount = amt_in # Used all amt_in
+                        s.out_amount += x - x_left
+                        s.reserves[n] = [x_left, y + in_amount_left - admin_fee]
+                        s.admin_fee += admin_fee
+                        break
+
+                    else:
+                        # Go into next band
+                        admin_fee = (dy - y_dest) * self.admin_fee # total admin fee paid
+                        in_amount_left -= dy
+                        # Updates
+                        s.out_amount += x
+                        s.in_amount += dy
+                        s.admin_fee += admin_fee
+                        s.reserves[n] = [0, y + dy - admin_fee]
+
+                if s.n2 == self.min_band:
+                    # there is no more liquidity
+                    break
+
+                # Prepare next loop
+                s.n2 -= 1
+                x = self.bands_x[s.n2]
+                y = 0 
+
+            else:
+                # Going left <-
+
+                if y != 0:
+                    assert self.g(n) > 0
+                    x_dest = (self.inv(n) / self.g(n) - self.f(n)) - x # Amt required to clear this band
+                    dx = x_dest / (1 - self.fee) # Amt required to clear this band + fee
+
+                    if dx >= in_amount_left:
+                        # This is the last band
+                        x_dest = in_amount_left * (1 - self.fee)
+                        y_left = self.inv(n) / (self.f(n) + (x + x_dest)) - self.g(n) + EPSILON
+                        assert 0 < y_left < y
+                        admin_fee = (in_amount_left - x_dest) * self.admin_fee
+                        # Updates
+                        s.in_amount = amt_in # Used all amt_in
+                        s.out_amount += y - y_left
+                        s.reserves[n] = [x + in_amount_left - admin_fee, y_left]
+                        s.admin_fee += admin_fee
+                        break
+
+                    else:
+                        # Go into next band
+                        admin_fee = (dx - x_dest) * self.admin_fee # total admin fee paid
+                        in_amount_left -= dx
+                        # Updates
+                        s.out_amount += y
+                        s.in_amount += dx
+                        s.admin_fee += admin_fee
+                        s.reserves[n] = [x + dx - admin_fee, 0]
+
+                if s.n2 == self.max_band:
+                    # there is no more liquidity
+                    break
+
+                # Prepare next loop
+                s.n2 += 1
+                x = 0
+                y = self.bands_y[s.n2] 
+
+        return s
+
+    def swap(self, amt_in, y_in):
         """
-        @notice This is a soft liquidation.
+        @notice Swap tokens in pool. This is a soft liquidation.
+        @param amt amount of tokens to swap in
+        @param y_in whether collateral is being sold to the AMM (True) or bought from the AMM (False)
+        @return [amt_in, amt_out] actual amount swapped in and out
+        TODO add slippage tolerance
         """
-        return
+        assert amt_in > 0
+
+        # NOTE: amt is amount to swap IN
+        s = self._swap(amt_in, y_in)
+        
+        if s.in_amount == 0 or s.out_amount == 0:
+            return 0
+
+        if y_in:
+            self.admin_fees_y += s.admin_fee
+        else:
+            self.admin_fees_x += s.admin_fee
+
+        for n, r in s.reserves.items():
+            self.bands_x[n] = r[0]
+            self.bands_y[n] = r[1]
+
+        self.active_band = s.n2
+
+        return s.in_amount, s.out_amount
 
     def deposit(self, user, amount, n1, n2):
         N = n2 - n1 + 1
@@ -162,7 +316,7 @@ class LLAMMA:
         elif y==0:
             # highest possible price of band
             return self.p_c_up(n)
-        return (x + self.f(n)) * (y + self.g(n))
+        return (x + self.f(n)) / (y + self.g(n))
     
     def p(self):
         """
@@ -190,7 +344,7 @@ class LLAMMA:
         return (self.A - 1) * self.y0(n) * self.p_o_up(n) / self.p_o
     
     def inv(self, n):
-        return (self.x + self.f(n)) * (self.y + self.g(n))
+        return (self.bands_x[n] + self.f(n)) * (self.bands_y[n] + self.g(n))
 
     def inv_up(self, n):
         return self.p_o * self.A**2 * self.y0(n)**2
@@ -210,7 +364,7 @@ class LLAMMA:
         a = p_o * self.A
         b = p_o_up * (self.A-1) * x / p_o + p_o**2/p_o_up * self.A * y
         c = x * y
-        return (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+        return (b + (b**2 - 4*a*c)**0.5) / (2*a)
     
     def y0(self, n):
         """
@@ -221,11 +375,11 @@ class LLAMMA:
     def plot_reserves(self):
         """
         @notice Plot reserves in each band
-        NOTE: for now, assume collateral price is = AMM price, and crvUSD price = $1
+        NOTE: for now, assume collateral price is = oracle price, and crvUSD price = $1
         """
         band_range = range(self.min_band, self.max_band)
         bands_x = [self.bands_x[i] for i in band_range]
-        bands_y = [self.bands_y[i] * self.p() for i in band_range]
+        bands_y = [self.bands_y[i] * self.p_o for i in band_range]
         band_edges = [self.p_o_down(i) for i in band_range]
         band_widths = [self.band_width(i)*0.9 for i in band_range]
 
