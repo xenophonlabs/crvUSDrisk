b90192b787b854291b7f65db45701d445d38a6a2
Vishesh <cytonomy@gmail.com>
2023-10-26 11:45:02 -0400

    update

--- src/modules/slippage.py
+++ src/modules/slippage.py
@@ -7,9 +7,12 @@ class Slippage:
        pass     
 
     def collateral_auction(self,tokens_in,price,price_path):
+        # defined as token0/token1 eg ETHUSD so amount of USD per ETH
+        price
         
-        return 1
+        return tokens_in*price
     
     def stable_auction(self,tokens_in,price,price_path):
-          
-        return 1
+        # defined as token0/token1 eg ETHUSD so amount of USD per ETH
+        price
+        return tokens_in*price
