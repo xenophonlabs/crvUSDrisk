53214aa04503f60324b461894c10c7de6a6ee38d
Vishesh <cytonomy@gmail.com>
2023-11-07 09:57:58 -0500

    Update to price generator

--- src/sim/pricegenerator.py
+++ src/sim/pricegenerator.py
@@ -135,9 +135,13 @@ class PriceGenerator:
 
         # Set ticks and format dates on the x-axis if needed
         # Assuming that 'T' is total time and 'dt' is the time step
-        time_steps = np.arange(0, T/dt + 1, 1)
-        ax_left.set_xticks(time_steps[::int(24*10/dt)])  # Adjust this if your time steps are not hourly
-        ax_left.set_xticklabels(time_steps[::int(24*10/dt)], rotation=45)  # Rotate for better spacing
+        total_hours = int(T * 365 * 24)
+        time_steps = np.arange(0, total_hours + 1, 1)  # Every hour
+        ticks_per_day = 24
+        day_interval = ticks_per_day / dt  # Number of ticks per day
+        # tick_labels = [f"Day {int(i/ticks_per_day)}" for i in range(0, total_hours + 1, int(day_interval))]
+        # ax_left.set_xticks(range(0, total_hours + 1, int(day_interval)))
+        # ax_left.set_xticklabels(tick_labels, rotation=45)
 
         # Add labels and title
         ax_left.set_xlabel("Time Steps")
@@ -161,137 +165,3 @@ class PriceGenerator:
         plt.show()
         return 0
 
-
-#### Archive below this line ####
-    # def gen_cor_jump_gbm(
-    #     self,
-    #     n_assets,
-    #     T,
-    #     dt,
-    #     mu,
-    #     sigma,
-    #     S0,
-    #     cor_matrix,
-    #     jump_list,
-    #     jump_direction,
-    #     recovery_perc,
-    #     recovery_speed,
-    # ):
-    #     n_steps = int(T / dt)
-    #     # Generate uncorrelated Brownian motions
-    #     dW = np.sqrt(dt) * np.random.randn(n_steps, n_assets)
-
-    #     # List of ordered pairs (jump_size, probability)
-    #     jump_list = sorted(
-    #         jump_list, key=lambda x: x[1]
-    #     )  # Sort by jump probability ascending
-
-    #     # Apply Cholesky decomposition to get correlated Brownian motions
-    #     L = np.linalg.cholesky(cor_matrix)
-    #     dW_correlated = dW.dot(L.T)
-
-    #     # Initialize asset prices
-    #     S = np.zeros((n_steps, n_assets))
-    #     S[0] = S0
-
-    #     recovery_period = 0
-    #     jump_to_recover = 0
-    #     jump_up_or_down = 0
-
-    #     # Generate GBM paths
-    #     for t in range(1, n_steps):
-    #         if recovery_period == 0:
-    #             # Generate a random number to decide if a jump occurs
-    #             rand_num = np.random.rand()
-
-    #             # jump diffusion based on poisson process
-    #             # @TODO: need to optimize the loop below
-    #             for size, prob in jump_list:
-    #                 if rand_num < prob:
-    #                     jump_up_or_down = random.choice(jump_direction)
-    #                     S[t] = S[t - 1] * (1 + size * jump_up_or_down)
-    #                     jump_to_recover = -1 * jump_up_or_down * size
-    #                     recovery_period = recovery_speed
-    #                     break
-    #                 else:
-    #                     S[t] = S[t - 1] * np.exp(
-    #                         (mu - 0.5 * sigma**2) * dt + sigma * dW_correlated[t]
-    #                     )
-    #         else:
-    #             recovery_period -= 1
-    #             S[t] = S[t - 1] * (1 + recovery_perc * jump_to_recover / recovery_speed)
-
-    #     return S
-    
-    # def gen_single_gbm(self, S0, mu, sigma, dt, T):
-    #     W = np.random.normal(loc=0, scale=np.sqrt(dt), size=int(T / dt))
-    #     S = S0 * np.exp(np.cumsum((mu - 0.5 * sigma**2) * dt + sigma * W))
-    #     return S
-
-    # def gen_single_jump_gbm(
-    #     self,
-    #     S0,
-    #     mu,
-    #     sigma,
-    #     dt,
-    #     T,
-    #     jump_list,
-    #     jump_direction,
-    #     recovery_perc,
-    #     recovery_speed,
-    # ):
-    #     n_steps = int(T / dt)
-    #     # List of ordered pairs (jump_size, probability)
-    #     jump_list = sorted(
-    #         jump_list, key=lambda x: x[1]
-    #     )  # Sort by jump probability ascending
-
-    #     # Initialize asset prices
-    #     S = np.zeros(n_steps)
-    #     S[0] = S0
-
-    #     recovery_period = 0
-    #     jump_to_recover = 0
-    #     jump_up_or_down = 0
-
-    #     # Generate GBM paths
-    #     for t in range(1, n_steps):
-    #         # @ TODO: need to optimize the loop below and decide whether we want to limit number of jumps
-    #         if recovery_period == 0:
-    #             for size, prob in jump_list:
-    #                 # Generate a random number to decide if a jump occurs
-    #                 rand_num = np.random.rand()
-    #                 if rand_num < prob:
-    #                     # 1 = up, -1 = down
-    #                     jump_up_or_down = random.choice(jump_direction)
-    #                     S[t] = S[t - 1] * (1 + size * jump_up_or_down)
-    #                     jump_to_recover = -1 * jump_up_or_down * size
-    #                     recovery_period = recovery_speed
-    #                     break
-    #                 else:
-    #                     W = np.random.normal(loc=0, scale=np.sqrt(dt))
-    #                     S[t] = S[t - 1] * np.exp((mu - 0.5 * sigma**2) * dt + sigma * W)
-    #         else:
-    #             recovery_period -= 1
-    #             S[t] = S[t - 1] * (1 + recovery_perc * jump_to_recover / recovery_speed)
-    #     return S
-
-    # def gen_cor_gbm(self, n_assets, n_steps, dt, mu, sigma, S0, cor_matrix):
-    #     # Generate uncorrelated Brownian motions
-    #     dW = np.sqrt(dt) * np.random.randn(n_steps, n_assets)
-
-    #     # Apply Cholesky decomposition to get correlated Brownian motions
-    #     L = np.linalg.cholesky(cor_matrix)
-    #     dW_correlated = dW.dot(L.T)
-
-    #     # Initialize asset prices
-    #     S = np.zeros((n_steps, n_assets))
-    #     S[0] = S0
-
-    #     # Generate GBM paths
-    #     for t in range(1, n_steps):
-    #         S[t] = S[t - 1] * np.exp(
-    #             (mu - 0.5 * sigma**2) * dt + sigma * dW_correlated[t]
-    #         )
-
-    #     return S
