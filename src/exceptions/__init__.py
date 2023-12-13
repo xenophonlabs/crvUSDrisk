ec8ac0dfe66113d1696b542d9986080516c1a3be
Thomas Cintra <tncintra@gmail.com>
2023-12-13 20:02:06 -0300

    Remove unused files

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -5,9 +5,4 @@ class crvusdRiskException(Exception):
     """Base class for exceptions in this module."""
 
 
-class coingeckoRateLimitException(crvusdRiskException):
-    """Coingecko API Rate Limit Exceeded."""
-
-
-class ccxtInvalidSymbolException(crvusdRiskException):
-    """Exchange does not support input symbol."""
+# TODO
